0e9451388456348d59c330e81cfaf2613b805e30
reyan <reyanonetwothree@gmail.com>
2023-11-03 16:53:09 +0530

    javascript

---
+++ javascript/sacript.js
@@ -0,0 +1,9 @@
+var naame = prompt("what is your name?");
+
+var naam = naame.toLowerCase();
+
+if (naam == "reyan") {
+  alert(`yes ${naam} you are smart and cool :)))`);
+} else {
+  alert(`no ${naam} you are not smart and cool ;(((`);
+}
