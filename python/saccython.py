5a74502e3e66acc7f9c197a79501a4a4729a7374
reyan <reyanonetwothree@gmail.com>
2023-11-03 16:46:28 +0530

    python

---
+++ python/saccython.py
@@ -0,0 +1,8 @@
+name = input('what is your name: ')
+naam = name.lower()
+
+if naam == 'reyan':
+    print(f'yes {name} you are smart and cool :)))')
+
+else:
+    print(f'no {name} you are not smart and cool ;(((')
